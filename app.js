8afe5df2a6f20b2f4f814bd7f94aa0a79fb4f13b
Melek OLCAY <39502040+melekolcayy@users.noreply.github.com>
2021-01-31 17:58:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+el:'#vue-app',
+ data: {
+    output:''
+    
+        },
+        methods:{
+         readRefs:function(){
+             console.log(this.$refs);
+             this.output=this.$refs.input.value;
+      
+           }
+            
+        }
+    });       
+    
